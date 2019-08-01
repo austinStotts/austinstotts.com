20eff3735b4fb83440f0273f88f802635cdeadcc
austinStotts <austintstotts@gmail.com>
2019-08-01 17:27:58 -0500

    pm2 BULSHIT

--- server/index.js
+++ server/index.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('dist'));
+
+app.get('/test', (req, res) => {
+  res.status(200).send("/test request: All Systems Online");
+})
+
 app.listen(4500, () => console.log('Roger Roger'));
