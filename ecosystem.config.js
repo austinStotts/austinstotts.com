e6fabc0ad76df986062eac6ef2f35f35c94cc1fd
austinStotts <austintstotts@gmail.com>
2019-08-01 17:13:41 -0500

    pm2 shit...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-18-208-59-63.compute-1.amazonaws.com',
       key: '~/.ssh/austinstotts.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:austinStotts/austinstotts.com.git',
+      repo: 'https://github.com/austinStotts/austinstotts.com.git',
       path: '/home/ubuntu/austinstotts',
       'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js'
     }
